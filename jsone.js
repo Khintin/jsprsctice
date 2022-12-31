1bc7053c2c77f07fa139d6ba0e260d15451b291b
Cristina Syv <91951394+Khintin@users.noreply.github.com>
2022-12-31 04:10:58 +0100

    done

--- jsone.js
+++ jsone.js
@@ -1,60 +1,37 @@
-/* question 1 
-Create an object called cat.
-
-Give the object one property called complain. complain's value should be a method (a function) which logs the string "Meow!".*/
+/* question 1:*/
 
 let cat = {
     complain: () => console.log("meow!"),
 
 };
 
-/*Question 2:
-
-Select the h3 from the HTML using the querySelector method and assign it to a variable called heading.
-
-Change its innerHTML value to "Updated heading".*/
+/*Question 2:*/
 
 let heading = document.querySelector("h3");
 heading.innerHTML = "Update heading";
 
-/*Question 3:
-
-Use the style property on the heading variable from the question above to change its font size to "2em".*/
+/*Question 3:*/
 
 heading.style.fontSize = "2em";
 
-/*Question 4:
-
-Add a class to the heading variable called subheading.*/
+/*Question 4:*/
 
 heading.classList.add = "subheading";
 
-/*Question 5:
-
-Write code that selects all the p elements on a page and assigns them to a variable called paragraphs.
-
-Loop through the p elements and change the colour of each to "red".*/
+/*Question 5:*/
 
 let paragraphs = document.querySelectorAll("p");
 paragraphs.forEach((paragraph) => {
     paragraph.style.color = "red";
 });
 
-/*Question 6:
-
-Select the div with a class of results, assign it to a variable called resultsContainer and set its inner HTML to be <p>New paragraph</p> and its background colour to be yellow.*/
+/*Question 6:*/
 
 let resultsContainer = document.querySelector(".results");
 resultsContainer.innerHTML = "<p>New paragraph<p>";
 resultsContainer.style.backgroundColor = "yellow";
 
-/*Question 7:
-
-Create a function that has one parameter called list.
-
-Inside the function, loop through the list parameter and console log the name property in each object.
-
-Call the function and pass in the cats variable in the script.js file in the repo.*/
+/*Question 7:*/
 
 const cats = [
     {
@@ -78,21 +55,7 @@ function logNames(list) {
 
 logNames(cats);
 
-/*Question 8:
-
-Create a function called createCats. The function will have one parameter called cats.
-
-Inside the function loop through the value passed in as cats and create HTML for each object in the array.
-
-Wrap each item in a div, each name property in an h5 tag and each age property in a p tag.
-
-If the age property is missing, it should display “Age unknown” instead.
-
-Return the HTML from the function.
-
-Call the function and pass in the cats array as the argument.
-
-Assign the return value of the function to the innerHTML property of the element on the HTML page with a class of cat-container.*/
+/*Question 8:*/
 
 function createCats(cats) {
     let html = "";
@@ -109,7 +72,15 @@ function createCats(cats) {
         if (cat.age) age.innerText = cat.age;
         else age.innerText = "AGE UNKNOWN";
 
-
+        wrapper.append(name, age);
+        html += wrapper.outerHTML;
 
     }
+    return html;
+
 }
+
+const catContainer = document.querySelector(".cat-container");
+catContainer.innerHTML = createCats(cats);
+
+
